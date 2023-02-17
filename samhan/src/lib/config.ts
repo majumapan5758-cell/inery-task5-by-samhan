4429c0329da3f82ecdcb934b1c96dca81c8b6bd2
El Samhan <74732752+majumapan5758-cell@users.noreply.github.com>
2023-02-17 22:39:51 +0700

    Update config.ts

--- samhan/src/lib/config.ts
+++ samhan/src/lib/config.ts
@@ -3,8 +3,8 @@ import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
-const private_key = '5JeAnutfUwSp2oK96Dpdy71Jm6vrnZUMVk1XauiSm6iZPdcepb9'; // this is my test account, don't worry
-export const actor = 'samhan.lite';
+const private_key = '5J3PjskAHUzXL1d6kbxh7fwaEfG6g8mfQS8QdZUizGRKZXWsku3'; // this is my test account, don't worry
+export const actor = 'SAM';
 
 export const account = 'samhan';
 const signature = new JsSignatureProvider([private_key]);
